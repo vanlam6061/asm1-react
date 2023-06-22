dc32607d433110b596a2dcae58e17c4d3783941f
vanlam6061 <119390647+vanlam6061@users.noreply.github.com>
2023-06-22 16:11:18 +0700

    a

--- src/pages/home/HomeFooter.js
+++ src/pages/home/HomeFooter.js
@@ -2,13 +2,18 @@
 
 
 import styles from '../../components/scss/home/HomeFooter/HomeFooter.module.scss'
-
+import HomeFooterRegister from './HomeFooter/HomeFooterRegister'
+import HomeFooterLinkTable from './HomeFooter/HomeFooterLinkTable'
 
 
 
 function HomeFooter (){
     return(
-        <div></div>
+        <div>
+            <HomeFooterRegister />
+            <HomeFooterLinkTable />
+
+        </div>
     )
 }
 export default HomeFooter
---
+++ src/pages/home/HomeFooter/HomeFooterRegister.js
@@ -0,0 +1,21 @@
+
+
+import  classNames from 'classname/bind'
+
+import styles from '../../components/scss/home/HomeFooter/HomeFooterRegister.module.scss'
+
+const cx = classNames.bind(styles)
+
+function HomeFooterRegister () {
+    return (
+        <div>
+            <p classNames = {cx('homeFooter--title')}>Save time, save money!</p>
+            <p>Sign up and we'll send the best deals to you</p>
+            <div classNames ={cx('homeFooter__containter')}>
+                <input type="text" classNames ={ cx('homeFooter__input')} placeholder="Your Email" />
+                <button classNames ={cx('button', 'homeFooter__button')}>Subcribe</button>
+            </div>
+        </div>
+        )
+}
+export default HomeFooterRegister
